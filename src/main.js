694b23c7c1126c79d28d5465b01467bde5a16b6f
Stefano Pisano <s.pisano@techgap.it>
2023-02-18 20:56:44 +0100

    feat(portfolio):  update to new version of gz-folio

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
-import router from './router/routes'
+import routes from './router/routes'
 import App from './App.vue'
 import {createApp} from "vue";
 import Loading from 'vue3-loading-screen'
 
-const app = createApp(App);
+import {library} from '@fortawesome/fontawesome-svg-core'
 
+/* import font awesome icon component */
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {faHeadphones} from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faHeadphones)
+
+
+const app = createApp(App);
 app.config.globalProperties.$theme = 'default'
+app.config.globalProperties.$routes = routes
+
 
 app.use(Loading, {
 	bg: '#41b883ad',
 	slot: `
 		<div class="px-5 py-3 bg-gray-800 rounded">
-      		<h3 class="text-3xl text-white"><img width="64" src="../loading.svg" alt="Loading..."/></h3>
+      		<h3 class="text-3xl text-white"><img width="64" src="./loading.svg" alt="Loading..."/></h3>
     	</div>
   `,
 	size: 3,
 	icon_color: 'white',
 });
 
-app.use(router).mount('#app');
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
